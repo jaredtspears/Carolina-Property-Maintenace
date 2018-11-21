032b1fdda91a8f9f34d19c37ef877fa30f289a53
Ha1f_Deaf <jaredtspears@gmail.com>
2018-11-21 12:11:24 -0500

    going with menu/sidebar for showcasing service, not functional yet - issue with .sidebar() from semantic or jquery

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+
+// for sidebar menu Tried to add js from semantic cdn at bottom of html page
+$("#toggle").click(function(){
+    $('.ui.left.sidebar').sidebar('toggle');
+});
+
+
+//  from a different example still testing this sidebar out
+// $('.ui.sidebar').sidebar({
+//     context: $('.bottom.segment')
+//   })
+//   .sidebar('attach events', '.menu .item');
